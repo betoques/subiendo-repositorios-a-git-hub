263f99df7c3095b3a65bfa32861a19838d8ac58f
bey <krishnamuertes23@gmail.com>
2023-04-28 01:20:29 -0600

    cambios en P4 y nueva practica P5

--- P4/javascript.js
+++ P4/javascript.js
@@ -1,10 +1,27 @@
-let numero = 5;
+// let numero = 5;
 
-let texto = 'saca las drogas mijares';
+// let texto = 'saca las drogas mijares';
 
-const number = 321;
+// const number = 321;
 
-let respuesta = true;
+// let respuesta = true;
 
-console.log(respuesta);
-console.log(texto);
+// console.log(numero);
+// console.log(texto);
+
+// console.log(Math.E+Math.PI);
+
+// let num = 0;
+
+// if(num>0){ 
+//     console.log(`${num} es mayor que 0`);
+
+// }else if(num<0){
+
+//     console.log(`${num} es menor que 0`);
+
+// }else{
+
+//     console.log(`${num} es igual que 0`);
+// }
+
